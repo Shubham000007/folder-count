c09661a363bc924cefd5e3bd3a7c5220c785e5d9
Shubham Rawat <shubham@claraerp.com>
2023-12-16 20:36:54 +0530

    folder images

---
+++ utils/folder-count.js
@@ -0,0 +1,29 @@
+import fs from "fs";
+import path from "path";
+
+function getFoldersAndImages(directory) {
+  const folderPath = path.join(process.cwd(), directory);
+
+  try {
+    const folders = fs
+      .readdirSync(folderPath)
+      .filter((file) => fs.statSync(path.join(folderPath, file)).isDirectory());
+
+    const folderData = folders.map((folder) => {
+      const folderImages = fs
+        .readdirSync(path.join(folderPath, folder))
+        .filter((file) => /\.(jpg|jpeg|png|gif)$/i.test(file));
+      return {
+        folderName: folder,
+        images: folderImages,
+      };
+    });
+
+    return folderData;
+  } catch (error) {
+    console.error("Error reading directory:", error.message);
+    return []; // Return an empty array to indicate an error or no folders/images
+  }
+}
+
+export default getFoldersAndImages;
